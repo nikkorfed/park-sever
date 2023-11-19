eacfbb1e0756e4477d97c8c3d6d7ce5374d2022a
Никита Корнилов <nikkorfed@gmail.com>
2023-11-19 17:56:38 +0500

    Disable animations. Adjust styles.

--- src/scripts/javascript/animations.js
+++ src/scripts/javascript/animations.js
@@ -7,31 +7,31 @@ function check() {
   $(".fadeIn").each(function () {
     if ($(this).hasClass("fading") || !$(this).hasClass("fadeIn")) return;
 
-    if (currentScroll > $(this).offset().top + 200) {
+    if (currentScroll > $(this).offset().top + 100) {
       $(this).addClass("fading");
       setTimeout(() => $(this).removeClass("fadeIn fading"), 1000);
     }
   });
 
-  $(".slideIn").each(function () {
-    function animate(element, delay = 0) {
-      if ($(element).hasClass("sliding") || !$(element).hasClass("slideIn")) return;
+  // $(".slideIn").each(function () {
+  //   function animate(element, delay = 0) {
+  //     if ($(element).hasClass("sliding") || !$(element).hasClass("slideIn")) return;
 
-      setTimeout(() => $(element).addClass("sliding"), delay);
-      setTimeout(() => $(element).removeClass("slideIn sliding"), delay + 500);
-    }
+  //     setTimeout(() => $(element).addClass("sliding"), delay);
+  //     setTimeout(() => $(element).removeClass("slideIn sliding"), delay + 500);
+  //   }
 
-    let delay = 0;
-    if (currentScroll > $(this).offset().top + 200) {
-      if (!$(this).hasClass("each")) return animate(this);
+  //   let delay = 0;
+  //   if (currentScroll > $(this).offset().top + 100) {
+  //     if (!$(this).hasClass("each")) return animate(this);
 
-      $(this).removeClass("slideIn each");
+  //     $(this).removeClass("slideIn each");
 
-      const elements = $(this).children();
-      elements.each(function () {
-        $(this).addClass("slideIn");
-        animate(this, (delay += 100));
-      });
-    }
-  });
+  //     const elements = $(this).children();
+  //     elements.each(function () {
+  //       $(this).addClass("slideIn");
+  //       animate(this, (delay += 100));
+  //     });
+  //   }
+  // });
 }
