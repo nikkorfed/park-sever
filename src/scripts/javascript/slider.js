0d81c2782ef185090e9fd87823e280ede4080158
Никита Корнилов <nikkorfed@gmail.com>
2023-11-25 16:01:07 +0500

    Add photos with animation to slider section.

---
+++ src/scripts/javascript/slider.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $("#slider .owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 5000,
+    animateIn: "fadeIn",
+    animateOut: "fadeOut",
+  });
+});
