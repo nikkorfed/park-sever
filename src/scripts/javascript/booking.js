2b9843c43b409bc68034f7605b01a05cb53fc0b3
Никита Корнилов <nikkorfed@gmail.com>
2023-12-22 12:33:19 +0500

    Switch to another booking module.

---
+++ src/scripts/javascript/booking.js
@@ -0,0 +1,24 @@
+window.znmsWidget.init("#znms-widget-1", {
+  moduleId: 5558,
+  index: 0,
+  widget: {
+    position: {
+      top: "100px",
+    },
+    mobile: {
+      absolute: false, // Абсолютное позиционирование в мобильной версии при виде отображения "У шапки сайта"
+      color: undefined, // HEX-код цвета виджета в мобильной версии
+      position: {
+        top: "100px",
+      },
+    },
+  },
+  // Настройки позиции плавающей кнопки
+  button: {
+    position: {
+      bottom: "50px",
+      left: "50px",
+      right: "50px",
+    },
+  },
+});
