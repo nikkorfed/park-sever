7d1273ab4e80d4a231d903b9bae4c6919c48c339
Никита Корнилов <nikkorfed@gmail.com>
2024-10-02 23:07:19 +0500

    Add news from VK.

---
+++ src/scripts/javascript/news.js
@@ -0,0 +1,2 @@
+// VK.init({ apiId: 52407743, onlyWidgets: true });
+VK.Widgets.Group("vk_groups", { mode: 4, height: 600, width: "auto", color1: "FFFFFF", color2: "000000", color3: "5181B8" }, 223976421);
